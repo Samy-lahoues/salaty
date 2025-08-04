25fe2bdf51f3a0665db74b406dcda775cd3bcea4
Samy Lahoues <samylahoues@outlook.com>
2025-08-04 21:15:19 +0100

    add: data from quran page data from a local json file

--- hooks/useQuran.ts
+++ hooks/useQuran.ts
@@ -1,56 +1,38 @@
 import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
+
+import surahsData from "@/constants/data.json";
 import { Surah } from "@/constants";
+
 export const useQuran = () => {
   const path = usePathname();
   const isQuran = path === "/quran";
-  const [surahs, setSurahs] = useState([]);
+  const [surahs, setSurahs] = useState<Surah[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
-  // const [reciter, setReciter] = useState("1")
-  const fetchSurahs = async () => {
-    const url = "https://quranapi.pages.dev/api/surah.json";
-    try {
-      setLoading(true);
-      const response = await fetch(url, {
-        headers: {
-          Accept: "application/json",
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Failed to fetch surahs");
-      }
-      const data = await response.json();
-      const surahsWithId = data.map((surah: Surah, index: number) => ({
-        ...surah,
-        id: index + 1,
-      }));
-      setSurahs(surahsWithId);
-    } catch (e) {
-      setError(e instanceof Error ? e.message : String(e));
-    } finally {
-      setLoading(false);
-    }
-  };
-  useEffect(() => {
-    fetchSurahs();
-  }, [isQuran]);
-  // const fetchSurah = async (id : number) => {
-  //   try {
 
-  //   }catch (e) {
-  //     setError(e)
-  //   }
-  // }
-  // const handleReciter = (reciter : number) => {
-  //   setReciter(String(reciter))
-  // }
-  // const playSurah = () => {
-  //   return
-  // }
-  // const playAya = () => {
-  //   return;
-  // }
+  useEffect(() => {
+    const loadSurahs = async () => {
+      try {
+        setLoading(true);
+        // Add IDs to each surah from JSON
+        const surahsWithId = surahsData.map((surah, index) => ({
+          ...surah,
+          id: index + 1,
+        }));
+        setSurahs(surahsWithId);
+      } catch (e) {
+        setError(e instanceof Error ? e.message : String(e));
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (isQuran) {
+      loadSurahs();
+    }
+  }, [isQuran]);
+
   return {
     surahs,
     loading,
